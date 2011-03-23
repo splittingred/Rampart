779137fd348ca8f7bd271c9973bbe313403483db
smooth-graphics <info@novolo.de>
2011-03-23 16:36:50 +0100

    added german translation
    Signed-off-by: smooth-graphics <info@novolo.de>

---
+++ core/components/rampart/lexicon/de/default.inc.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Rampart
+ *
+ * Copyright 2011 by Shaun McCormick <shaun@modx.com>
+ *
+ * Rampart is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Rampart is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Rampart; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package rampart
+ */
+/**
+ * @package rampart
+ * @subpackage lexicon-de
+ * @author: Anselm Hannemann (smooth-graphics), Novolo Designagentur (http://www.novolo.de/)
+ */
+$_lang['rampart'] = 'Rampart';
+$_lang['rampart.activate_selected'] = 'ausgewählte aktivieren';
+$_lang['rampart.active'] = 'Aktiv';
+$_lang['rampart.active_desc'] = 'zeigt an ob der Bann aktiv ist';
+$_lang['rampart.approve_selected'] = 'ausgewählte Registrierungen freischalten';
+$_lang['rampart.approved'] = 'Freigeschaltet';
+$_lang['rampart.attemptedon'] = 'versuchter Zugriff auf';
+$_lang['rampart.awaiting'] = 'warten auf Moderation';
+$_lang['rampart.ban'] = 'Bann';
+$_lang['rampart.ban_activate'] = 'Bann aktivieren';
+$_lang['rampart.ban_add_new'] = 'neuen Bann hinzufügen';
+$_lang['rampart.ban_add_desc'] = 'Bitte füllen Sie einige Muster für den Bann aus. Sie können ein E-Mail Muster, Hostname Muster, Benutzername Muster oder ein IP-Raum Muster angeben.';
+$_lang['rampart.ban_deactivate'] = 'Bann deaktivieren';
+$_lang['rampart.ban_duplicate'] = 'Bann duplizieren';
+$_lang['rampart.ban_err_nf'] = 'Bann ID "[[+id]]" konnte nicht gefunden werden';
+$_lang['rampart.ban_err_ns'] = 'Dieser Bann wurde nicht erstellt';
+$_lang['rampart.ban_err_save'] = 'Es trat ein Fehler beim Entfernen des Banns auf.';
+$_lang['rampart.ban_err_save'] = 'Es trat ein Fehler beim Speichern des banns auf.';
+$_lang['rampart.ban_group'] = 'Bann-Gruppe';
+$_lang['rampart.ban_matches'] = 'Bann-Treffer';
+$_lang['rampart.ban_matches.intro_msg'] = 'Hier sehen Sie eine Liste der Treffer, die auf Ihre Banns zutrafen.';
+$_lang['rampart.ban_remove'] = 'Bann entfernen';
+$_lang['rampart.ban_remove_confirm'] = 'Sind Sie sicher, dass Sie den Bann entfernen möchten?';
+$_lang['rampart.ban_remove_selected'] = 'ausgewählte Banns entfernen';
+$_lang['rampart.ban_remove_selected_confirm'] = 'Sind Sie sicher, dass Sie die ausgewählten Banns alle dauerhaft entfernen möchten?';
+$_lang['rampart.ban_update'] = 'Bann editieren';
+$_lang['rampart.bans'] = 'Banns';
+$_lang['rampart.bans.intro_msg'] = 'Verwalten Sie alle Ihre seitenweit festgelegten Banns hier. Ausgegraute Banns sind inaktiv und werden nicht überprüft.';
+$_lang['rampart.bulk_actions'] = 'Stapelaktionen';
+$_lang['rampart.createdon'] = 'Erstellt am';
+$_lang['rampart.deactivate_selected'] = 'ausgewählten deaktivieren';
+$_lang['rampart.email'] = 'E-Mail';
+$_lang['rampart.email_desc'] = 'E-Mail Muster zur Sperre. Sie können % als beliebige Zeichenkette nutzen.';
+$_lang['rampart.expireson'] = 'läuft aus am';
+$_lang['rampart.expireson_desc'] = 'Optional. Falls gesetzt, wird die Sperre ab diesem Datum deaktiviert.';
+$_lang['rampart.filter_by_status'] = 'nach Status filtern';
+$_lang['rampart.flag_approve'] = 'Registratrierung freischalten';
+$_lang['rampart.flag_ipusername'] = 'übereinstimmende IP/Benutzername';
+$_lang['rampart.flag_email'] = 'Spammer E-Mail';
+$_lang['rampart.flag_reject'] = 'Registrierung zurückweisen';
+$_lang['rampart.flag_sfsip'] = 'übereinstimmende StopForumSpam IP';
+$_lang['rampart.flaggedon'] = 'markiert am';
+$_lang['rampart.flaggedfor'] = 'markiert als';
+$_lang['rampart.hostname'] = 'Hostname';
+$_lang['rampart.hostname_desc'] = 'Hostnamen Muster zur Sperre. Sie können % als beliebige Zeichenkette nutzen.';
+$_lang['rampart.ip'] = 'IP';
+$_lang['rampart.ip_range'] = 'IP Raum';
+$_lang['rampart.ip_range_desc'] = 'Eine IP Adresse oder ein IP Raum zur Sperre. Sie können * als beliebige Zeichenkette für die einzelnen IP-Blöcke nutzen.<br /><br />Beispiele:<br /> 220.123.71.*<br /> 123.11.0-100.45<br /> 123.*.*.*<br /> 192.168.0.1';
+$_lang['rampart.match_view'] = 'Trefferdetails ansehen';
+$_lang['rampart.matches'] = 'Übereinstimmungen';
+$_lang['rampart.moderated_users'] = 'moderierte Benutzer';
+$_lang['rampart.moderated_users.intro_msg'] = 'Hier können Sie die Benutzerregistrierungen verwalten, die als potenzieller Spam markiert wurden. Sie können diese freischalten oder zurückweisen. Freischaltungen benötigen zusätzlich eine Freischaltung des Benutzers per E-Mail.';
+$_lang['rampart.menu_desc'] = 'Ihre eigene Anti-Spam Festung.';
+$_lang['rampart.notes'] = 'Notizen';
+$_lang['rampart.notes_desc'] = 'Optional. Sämtliche Notizen, die Sie zu diesem Bann hinzufügen möchten.';
+$_lang['rampart.reason'] = 'Grund';
+$_lang['rampart.reason_desc'] = 'Optional. Ein kurzer Grund für die Zurückweisung.';
+$_lang['rampart.reject_selected'] = 'ausgewählte Registrierungen zurückweisen';
+$_lang['rampart.rejected'] = 'Zurückgewiesen';
+$_lang['rampart.remove_selected'] = 'ausgewählte entfernen';
+$_lang['rampart.resource'] = 'Ressource';
+$_lang['rampart.useragent'] = 'User Agent';
+$_lang['rampart.username'] = 'Benutzername';
+$_lang['rampart.username_desc'] = 'Ein Benutzernamens-Schema für den Bann. Sie können % als beliebige Zeichenkette nutzen.';
+
+$_lang['setting_rampart.sfs_ipban_threshold'] = 'StopForumSpam IP Bann Grenzwert';
+$_lang['setting_rampart.sfs_ipban_threshold_desc'] = 'Wenn eine IP Adresse im StopForumSpam öfter als x-mal als Spam markiert wurde, kann ein automatischer Bann für diese IP festgelegt werden. Setzen Sie den Wert auf \'0\' um diese Funktion nicht zu nutzen.';
+$_lang['setting_rampart.sfs_ipban_expiration'] = 'StopForumSpam IP Bann Verfall';
+$_lang['setting_rampart.sfs_ipban_expiration_desc'] = 'Anzahl der Tage, für die ein IP Bann von StopForumSpam gültig ist.';
+
+$_lang['setting_rampart.honeypot.enabled'] = 'ProjectHoneyPot Blacklist aktivieren';
+$_lang['setting_rampart.honeypot.enabled_desc'] = 'Falls aktiviert, wird ein Blacklist Bann für Ihre Seite via Project Honey Pot aktiviert.';
+$_lang['setting_rampart.honeypot.access_key'] = 'ProjectHoneyPot Access Key';
+$_lang['setting_rampart.honeypot.access_key_desc'] = 'Der Access Key für Ihren HoneyPot Account. Dieser wird benötigt, um den Service nutzen zu können.';
+$_lang['setting_rampart.honeypot.ban_expiration'] = 'ProjectHoneyPot Bann Verfall';
+$_lang['setting_rampart.honeypot.ban_expiration_desc'] = 'Anzahl der Tage, für die ein Eintrag in der ProjectHoneypot Blacklist gültig ist.';
+$_lang['setting_rampart.honeypot.blacklist_message'] = 'ProjectHoneyPot Blacklist Bann Nachricht';
+$_lang['setting_rampart.honeypot.blacklist_message_desc'] = 'Die anzuzeigende Nachricht für IPs, die in der HoneyPot Blacklist eingetragen sind.';
+$_lang['setting_rampart.honeypot.blhost'] = 'ProjectHoneyPot Blacklist Host';
+$_lang['setting_rampart.honeypot.blhost_desc'] = 'Der Hostname für den Project Honey Pot Blacklist Service.';
